30a682ee3c6855cd546a7839538489eac51bb146
Shaun Tabone <shaun.tabone@gig.com>
2019-11-08 17:31:04 +0100

    Added build args

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
 #/bin/sh
-if [ -z "$1" ]
+if [ -z "$GH_TOKEN" ]
 then
       echo "Skipping NuGet Push..."
 else
-      dotnet nuget push /sln/**/*.nupkg -s "https://nuget.pkg.github.com/cdemi/index.json" -k $1:$2
+      nuget
+      dotnet nuget push /sln/artifacts/*.nupkg -s "https://nuget.pkg.github.com/cdemi/index.json" -k $GH_TOKEN
 fi
